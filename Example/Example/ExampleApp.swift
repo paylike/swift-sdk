f07aa47b517a67e70625b80a760a3dcda4ba64ba
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-04-25 13:31:54 +0200

    feat(example): created example skeleton with default example app implementation

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Laszlo Kocsis on 2023. 04. 20..
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
